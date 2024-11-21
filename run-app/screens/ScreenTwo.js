1bbc9f13365034918c5bc99a2c3ddb0246953105
kasper kammer <kasperkammer@gmail.com>
2024-11-21 10:26:39 +0100

    store and add icon

--- run-app/screens/ScreenTwo.js
+++ run-app/screens/ScreenTwo.js
@@ -1,7 +1,10 @@
 // screens/ScreenTwo.js
 import { PanGestureHandler } from "react-native-gesture-handler";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import runner from "../assets/video_preview_0000.avif";
 
 function ScreenTwo({ navigation }) {
   return (
@@ -17,11 +20,68 @@ function ScreenTwo({ navigation }) {
         }
       }}
     >
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Screen Two</Text>
+      <View style={styles.container}>
+        <View style={styles.card}>
+          <Text style={styles.cardText}>Card 1</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardText}>Card s1</Text>
+        </View>
+        <View style={styles.card}>
+          <Image source={require("../assets/video_preview_0000.avif")} style={styles.Image} blurRadius={20} />
+          <FontAwesomeIcon icon={faLock} size="48" style={styles.lockImage} />
+        </View>
+        <View style={styles.card}>
+          <Image source={require("../assets/video_preview_0000.avif")} style={styles.Image} />
+        </View>
       </View>
     </PanGestureHandler>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    justifyContent: "center", // Center the row
+    alignItems: "center", // Vertically align items
+    flexWrap: "wrap",
+  },
+  card: {
+    marginHorizontal: 5, // Horizontal spacing between cards
+    marginVertical: 5, // Vertical spacing
+    backgroundColor: "#fff",
+
+    borderRadius: 10,
+    width: "45%",
+    height: "25%",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  cardText: {
+    fontSize: 16,
+    color: "#333",
+  },
+  Image: {
+    width: "100%",
+    height: "100%",
+  },
+  lockImage: {
+    fontSize: 48,
+    width: 200,
+    height: 200,
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -25 }, { translateY: -25 }],
+    position: "absolute",
+    //bottom: 10, // Positions lock near the bottom of the base image
+    //right: 10, // Positions lock near the right side
+    backgroundColor: "transparent",
+  },
+});
 export default ScreenTwo;
